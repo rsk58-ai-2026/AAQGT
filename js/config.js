cc0efb3bf5b0a6184a797a0b3978cf5ea8a3dee1
rsk58-ai-2026 <kitako.58rsk@gmail.com>
2026-08-31 15:41:21 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // 1. Google Apps Script のデプロイURL
   // ※ステップ1でデプロイした「ウェブアプリのURL」をここに貼り付けてください
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbytbFBuutm6a4tyg9Ni3AOwzu7VhmesVtTHiv0B8EpGKT6U5RcqwVznUof6qJZzGa7a/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyT2HilVPYZHSvOQML-cJ5YH5tGmtNBJoyBSfut7PXn7QOxT6QeoDGMEpCjQVFX38j7/exec',
 
   // 2. 端末の役割定義
   ROLES: {
